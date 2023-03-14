ec3c9222c32fe5bd9c14e72f7fc554c9f1f155ff
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-03-14 17:17:18 +0700

    Menambahkan class PosisiKarakterGame

---
+++ MOD4_13022101011/MOD4_13022101011/PosisiKarakterGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD4_13022101011
+{
+    internal class PosisiKarakterGame
+    {
+        public enum State { Tengkurap, Jongkok, Berdiri, Terbang}
+
+        public enum Trigger { TombolS,TombolW }
+
+        public State current;
+
+    }
+}
--- MOD4_13022101011/MOD4_13022101011/Program.cs
+++ MOD4_13022101011/MOD4_13022101011/Program.cs
@@ -4,3 +4,42 @@ using MOD4_13022101011;
 KodeBuah buah = new KodeBuah();
 KodeBuah.Buah1 apl = KodeBuah.Buah1.Apel;
 Console.WriteLine(apl + " " + buah.getKodeBuah(apl));
+
+PosisiKarakterGame pos = new PosisiKarakterGame();
+pos.current = PosisiKarakterGame.State.Berdiri;
+string[] screen = { "Tengkurap", "Jongkok", "Berdiri", "Terbang" };
+while(pos.current != PosisiKarakterGame.State.Tengkurap)
+{
+    Console.WriteLine(screen[(int)pos.current]);
+    Console.WriteLine("Enter command :");
+
+    string command = Console.ReadLine();
+    switch (pos.current)
+    {
+        case PosisiKarakterGame.State.Berdiri:
+            if (command == "TombolW")
+                pos.current = PosisiKarakterGame.State.Terbang;
+            else if (command == "TombolS")
+                pos.current = PosisiKarakterGame.State.Jongkok;
+            break;
+        case PosisiKarakterGame.State.Jongkok:
+            if (command == "TombolS")
+                pos.current = PosisiKarakterGame.State.Tengkurap;
+            else if (command == "TombolW")
+                pos.current = PosisiKarakterGame.State.Berdiri;
+            break;
+        case PosisiKarakterGame.State.Terbang:
+            if (command == "TombolW")
+                pos.current = PosisiKarakterGame.State.Terbang;
+            else if (command == "TombolS")
+                pos.current = PosisiKarakterGame.State.Berdiri;
+            break;
+        case PosisiKarakterGame.State.Tengkurap:
+            if (command == "TombolW")
+                pos.current = PosisiKarakterGame.State.Berdiri;
+            else if (command == "TombolS")
+                pos.current = PosisiKarakterGame.State.Tengkurap;
+            break;
+    }
+    
+}
