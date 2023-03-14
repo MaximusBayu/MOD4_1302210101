f8e83daf0a5d9469e2f04ccb1ed9316791484b2a
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-03-14 16:27:59 +0700

    ganti project (Menambah Class Kode Buah)

---
+++ MOD4_13022101011/MOD4_13022101011/KodeBuah.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD4_13022101011
+{
+    internal class KodeBuah
+    {
+        public enum Buah1
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung
+        }
+
+        public enum Buah2
+        {
+            Kurma, Durian, Anggur, Melon, Semangka
+        }
+
+        public string getKodeBuah(Buah1 kb)
+        {
+
+            string[] KodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+
+            return KodeBuah[(int)kb];
+        }
+    }
+}
---
+++ MOD4_13022101011/MOD4_13022101011/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MOD4_13022101011;
+
+KodeBuah buah = new KodeBuah();
+KodeBuah.Buah1 apl = KodeBuah.Buah1.Apel;
+Console.WriteLine(apl + " " + buah.getKodeBuah(apl));
